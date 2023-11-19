b3f4966dfcb47eb28ffb0ffad51c43c5f2d34ff5
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-11-19 11:18:54 +0000

    update packages; make nav work ok with more items in it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+	integrations: [tailwind()],
 });
