6babc53e683990f0faa0608a526d9bf2d88c6537
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-11-19 12:08:27 +0000

    change to @ ts alias

--- src/utils/fetchWordpressPage.ts
+++ src/utils/fetchWordpressPage.ts
@@ -1,11 +1,11 @@
-import { WordpressURL } from "../../siteData";
+import { WordpressURL } from "@/siteData";
 export const fetchWordpressPage = async (WordPressPageID: string) => {
-  const { title, content } = await fetch(
-    `https://public-api.wordpress.com/rest/v1.1/sites/${WordpressURL}/posts/${WordPressPageID}`
-  ).then((res) => res.json());
+	const { title, content } = await fetch(
+		`https://public-api.wordpress.com/rest/v1.1/sites/${WordpressURL}/posts/${WordPressPageID}`
+	).then((res) => res.json());
 
-  return {
-    title,
-    content,
-  };
+	return {
+		title,
+		content,
+	};
 };
