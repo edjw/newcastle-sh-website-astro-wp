c16681f74ad5afc3eead60da0555487ef8884104
Ed Johnson-Williams <edjw@users.noreply.github.com>
2025-04-28 12:53:08 +0100

    tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,40 @@
 import { defineConfig, envField } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
+
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
-	integrations: [tailwind()],
-	env: {
-		schema: {
-			WORDPRESS_URL: envField.string({
-				context: "server",
-				access: "public"
-			}),
-			WORDPRESS_CLIENT_ID: envField.string({
-				context: "server",
-				access: "secret"
-			}),
-			WORDPRESS_CLIENT_SECRET: envField.string({
-				context: "server",
-				access: "secret"
-			}),
-			WORDPRESS_USERNAME: envField.string({
-				context: "server",
-				access: "secret"
-			}),
-			WORDPRESS_PASSWORD: envField.string({
-				context: "server",
-				access: "secret"
-			}),
-			WORDPRESS_USE_AUTH: envField.boolean({
-				context: "server",
-				access: "public"
-			}),
-		}
-	}
+  env: {
+      schema: {
+          WORDPRESS_URL: envField.string({
+              context: "server",
+              access: "public"
+          }),
+          WORDPRESS_CLIENT_ID: envField.string({
+              context: "server",
+              access: "secret"
+          }),
+          WORDPRESS_CLIENT_SECRET: envField.string({
+              context: "server",
+              access: "secret"
+          }),
+          WORDPRESS_USERNAME: envField.string({
+              context: "server",
+              access: "secret"
+          }),
+          WORDPRESS_PASSWORD: envField.string({
+              context: "server",
+              access: "secret"
+          }),
+          WORDPRESS_USE_AUTH: envField.boolean({
+              context: "server",
+              access: "public"
+          }),
+      }
+	},
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
